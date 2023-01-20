28f628d62d8158d95b937d84247c9f424ebc6087
dnskakun <dimon_sdn@list.ru>
2023-01-20 12:26:52 +0300

    Решение задачи №1

---
+++ Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 41. Пользователь вводит с клавиатуры М чисел.
+// Посчитайте, сколько чисел больше 0 ввел пользователь.
+// 0, 7, 8, -2, -3 -> 2
+
+Console.Clear();
+
+int number = GetNumberFromUser();
+GetQuantNumber(number);
+
+
+//Функции, используемые в программе
+int GetNumberFromUser()
+{
+    while (true)
+    {
+        Console.Write("Введите количество вводимых чисел: ");
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int num);
+        if (isCorrect)
+            return num;
+        else Console.WriteLine("Ошибка ввода!");
+    }
+}
+
+void GetQuantNumber(int num)
+{
+    int[] array = new int[num];
+    int count = 0;
+    for (int i = 0; i < num; i++)
+    {
+        while (true)
+        {
+            Console.Write($"Введите {i+1}-е число: ");
+            bool isCorrect = int.TryParse(Console.ReadLine(), out int number);
+            if (isCorrect)
+            {
+                array[i] = number;
+                if (array[i] > 0) count++;
+                break;
+            }
+            else Console.WriteLine("Ошибка ввода!");
+        }
+    }
+    Console.WriteLine($"{String.Join(", ", array)} -> {count}");
+}
